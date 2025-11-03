124411fcc23f51ed8b1e1c64987dae4ed1639f8c
JeonByeol <star1004szx@naver.com>
2025-11-03 10:43:16 +0900

    Controller/Service 분리 리팩토링

--- backend/src/main/java/com/univercity/unlimited/greenUniverCity/controller/UserController.java
+++ backend/src/main/java/com/univercity/unlimited/greenUniverCity/controller/UserController.java
@@ -4,6 +4,7 @@ import com.univercity.unlimited.greenUniverCity.dto.UserDTO;
 import com.univercity.unlimited.greenUniverCity.entity.UserRole;
 import com.univercity.unlimited.greenUniverCity.entity.UserVo;
 import com.univercity.unlimited.greenUniverCity.repository.UserRepository;
+import com.univercity.unlimited.greenUniverCity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +19,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
-    private final UserRepository userRepository;
-
+//    private final UserRepository userRepository;
+    private final UserService userService;
     @GetMapping("/all")
     public List<UserVo> postmanTestUser(){
-       return userRepository.findAll();
+        log.info("Controller: /api/user/all 호출");
+        return userService.findAllUsers();
     }
     @GetMapping("/role/{roleName}")
     public List<UserVo> postmanTestRole(@PathVariable("roleName") UserRole role){
-        return userRepository.findAllByRole(role);
+        log.info("Controller: /api/user/role/{} 호출", role);
+        return userService.findUsersByRole(role);
     }
 
 }
