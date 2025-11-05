108f22c54e8f03f631fa0896a1cb1aae0c353134
Kihyun <gihyun7070@gmail.com>
2025-11-05 17:01:39 +0900

    attendance_notice_repositorytest 내용수정
     attendance_notice_repositorytest 내용수정

--- backend/src/test/java/com/univercity/unlimited/greenUniverCity/repository/AttendanceRepositoryTests.java
+++ backend/src/test/java/com/univercity/unlimited/greenUniverCity/repository/AttendanceRepositoryTests.java
@@ -1,17 +1,30 @@
 package com.univercity.unlimited.greenUniverCity.repository;
 
+import com.univercity.unlimited.greenUniverCity.entity.Attendance;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 @SpringBootTest
 @Slf4j
 public class AttendanceRepositoryTests {
     @Autowired
-    private AttendanceRepository attendanceRepositoryr;
+    private AttendanceRepository attendanceRepository;
 
-    @Autowired
-    private EnrollmentRepository enrollmentRepository;
+    @Test
+    public void testInsertData() {
+        for (int i = 0; i < 1; i++) {
+            Attendance attendance = Attendance.builder()
+                    .attendance(i)
+                    .enrollment(null)
+                    .localDate(LocalDate.now())
+                    .status("출석")
+                    .build();
+            attendanceRepository.save(attendance);
+        }
+    }
 
 }
