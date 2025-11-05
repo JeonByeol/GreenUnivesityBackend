7f334dfdc16dafda72c484873101e2b4cc623cd7
Kihyun <gihyun7070@gmail.com>
2025-11-05 13:16:27 +0900

    attendance,notice관련 클래스생성
    <entity>
    Attendance
    Notice
    <repository>
    AttendanceRepository
    NoticeRepository
    <test→repository>
    AttendanceRepositoryTests
    NoticeRepositoryTests
    <controller>
    AttendanceController
    NoticeController
    생성

---
+++ backend/src/main/java/com/univercity/unlimited/greenUniverCity/repository/NoticeRepository.java
@@ -0,0 +1,8 @@
+package com.univercity.unlimited.greenUniverCity.repository;
+
+import com.univercity.unlimited.greenUniverCity.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository <Notice,String>{
+    //uservo의 타입
+}
