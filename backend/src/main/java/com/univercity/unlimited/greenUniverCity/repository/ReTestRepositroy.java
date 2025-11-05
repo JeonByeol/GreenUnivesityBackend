000728ecaa6026895111c237579bccf1e1e314dc
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-05 16:41:24 +0900

    엔티티 추가 및 테스트 환경 세팅
    1. Table이름 수정 완료
    2. NoArgsCOnstructor추가
    3. 주석 추가
    4. 테스트추가
    5. 레파지토리추가

--- backend/src/main/java/com/univercity/unlimited/greenUniverCity/repository/ReTestRepositroy.java
+++
@@ -1,7 +0,0 @@
-package com.univercity.unlimited.greenUniverCity.repository;
-
-import com.univercity.unlimited.greenUniverCity.entity.ReTestVo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReTestRepositroy extends JpaRepository<ReTestVo,Long> {
-}
