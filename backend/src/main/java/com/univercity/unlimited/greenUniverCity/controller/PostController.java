ce9107d2e5b3bca7aca87f13dbdcca38a1d77312
김유라 <dbfk0214@gmail.com>
2025-11-07 18:03:16 +0900

    Post commit
    Post commit

--- backend/src/main/java/com/univercity/unlimited/greenUniverCity/controller/PostController.java
+++ backend/src/main/java/com/univercity/unlimited/greenUniverCity/controller/PostController.java
@@ -1,31 +1,39 @@
-//package com.univercity.unlimited.greenUniverCity.controller;
-//
-//import com.univercity.unlimited.greenUniverCity.entity.Course;
-//import com.univercity.unlimited.greenUniverCity.entity.Post;
-//import com.univercity.unlimited.greenUniverCity.service.PostService;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@Slf4j
-//@RequiredArgsConstructor
-//@RequestMapping("/community/post")
-//public class PostController {
-//
-//    @GetMapping("/All")
-//    public List<Post> postAllSearch(){
-//        log.info("모든 강의들의 정보 호출");
-//        return PostService.findAllPost();
-//    }
-//    @GetMapping("/Search")
-//    public List<Post> postSearch() {
-//        log.info("특정 회원의 정보 호출");
-//        return PostService.findById();
-//    }
-//}
+package com.univercity.unlimited.greenUniverCity.controller;
+
+import com.univercity.unlimited.greenUniverCity.dto.CommentDTO;
+import com.univercity.unlimited.greenUniverCity.dto.PostDTO;
+import com.univercity.unlimited.greenUniverCity.entity.Comment;
+import com.univercity.unlimited.greenUniverCity.service.CommentService;
+import com.univercity.unlimited.greenUniverCity.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.channels.ReadPendingException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/Post")
+public class PostController {
+
+    private final PostService postService;
+    @GetMapping("/read/{cid}")
+    public ResponseEntity<PostDTO> getReadCommentByCid(@PathVariable("cid") Long PostId) {
+        log.info("Controller: /community/read 호출");
+        PostDTO result = postService.findAllPost(PostDTO->postService.findByIdPost(PostId));
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/list/Search")
+    public ResponseEntity<List<CommentDTO>> getListPosts() {
+        log.info("1) Controller: /community/list 호출");
+        List<CommentDTO> result  = postService.findByIdPost();
+        log.info("4) Controller: /community,{}" ,result);
+        return ResponseEntity.ok(result);
+    }
+}
+
