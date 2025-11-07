c71978d84494299f38b6edd66b4b351932a505d1
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-11-07 09:41:57 +0900

    fixed
    post 추가
    postrepo수정

--- backend/src/test/java/com/univercity/unlimited/greenUniverCity/repository/PostRepositoryTests.java
+++ backend/src/test/java/com/univercity/unlimited/greenUniverCity/repository/PostRepositoryTests.java
@@ -1,49 +1,47 @@
-//package com.univercity.unlimited.greenUniverCity.repository;
-//import com.univercity.unlimited.greenUniverCity.entity.*;
-//import lombok.ToString;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@SpringBootTest
-//@Slf4j
-//@ToString
-//public class PostRepositoryTests {
-//
-//    @Autowired private PostRepository postRepository;
-//    @Autowired private BoardRepository boardRepository;
-//    @Autowired private UserRepository userRepository;
-//
-//    @Test
-//    public void testInsert(){
-//        List<UserVo> users = userRepository.findAll();
-////        List<Board> board = boardRepository.findAll();
-//        Board board = boardRepository.getReferenceById(1L);
-//        users.forEach(u->{
-//                log.info("{}",u);
-//                for(int i = 0; i<20; i++){
-//                    Post dto = Post.builder()
-//        //                    .postId(""+i)
-////                            .boardId(10l+i)
+package com.univercity.unlimited.greenUniverCity.repository;
+import com.univercity.unlimited.greenUniverCity.entity.*;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+@ToString
+public class PostRepositoryTests {
+
+    @Autowired private PostRepository postRepository;
+    @Autowired private BoardRepository boardRepository;
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    public void testInsert(){
+        List<UserVo> users = userRepository.findAll();
+//        List<Board> board = boardRepository.findAll();
+        Board board = boardRepository.getReferenceById(1L);
+        users.forEach(u->{
+                log.info("{}",u);
+                for(int i = 0; i<20; i++){
+                    Post dto = Post.builder()
 //                            .board(board)
 //                            .user(u)
-////                            .userId(12l+i)
-//                            .title("title"+i)
-//                            .content("content"+i)
-//                            .createdAt(LocalDateTime.now())
-//                            .viewCount((int) (1l+i))
-//                            .build();
-//                    postRepository.save(dto);
-//                }
-//            });
-//    }
-//    @Test
-//    public void testRead(){
-//        List<Post> result = postRepository.findAll();
-//        log.info("hello={}",result);
-//    }
-//}
+                            .userId(null)
+                            .title("title"+i)
+                            .content("content"+i)
+                            .createAt(LocalDateTime.now())
+                            .viewCount(i)
+                            .build();
+                    postRepository.save(dto);
+                }
+            });
+    }
+    @Test
+    public void testRead(){
+        List<Post> result = postRepository.findAll();
+        log.info("hello={}",result);
+    }
+}
