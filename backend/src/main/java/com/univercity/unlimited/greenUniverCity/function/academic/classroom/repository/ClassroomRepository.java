4bb381234793ababc0550c74f48898e166c07874
JoheoungWoo <hataraci9350@daum.net>
2025-12-04 15:30:00 +0900

    Classroom 미완성으로 인한 실행안됨
    임시로 Query 대강 적어놓았습니다.
    추후 수정바랍니다.

--- backend/src/main/java/com/univercity/unlimited/greenUniverCity/function/academic/classroom/repository/ClassroomRepository.java
+++ backend/src/main/java/com/univercity/unlimited/greenUniverCity/function/academic/classroom/repository/ClassroomRepository.java
@@ -2,10 +2,13 @@ package com.univercity.unlimited.greenUniverCity.function.academic.classroom.rep
 
 import com.univercity.unlimited.greenUniverCity.function.academic.classroom.entity.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ClassroomRepository extends JpaRepository<Classroom,Long> {
     //CR-2 특정 강의실에 대한 정보를 조회하는 서비스 구현부에서 location의 값을 전달하기 위해 쿼리문 작성
-    List<Classroom> findByLocationContain(String keyword);
+    @Query("SELECT c FROM Classroom c WHERE c.location LIKE %:keyword%")
+    List<Classroom> findByLocationContain(@Param("keyword") String keyword);
 }
