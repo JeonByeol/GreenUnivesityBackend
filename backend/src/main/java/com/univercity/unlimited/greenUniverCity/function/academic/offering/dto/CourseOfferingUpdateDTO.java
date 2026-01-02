4964a4e605b275ec3997a2d362c886749df3854b
JoheoungWoo <hataraci9350@daum.net>
2026-01-02 15:21:54 +0900

    Offering 수정해서 과목관리 추가했습니다.
    건드리면서, 이것저것 다 엮여있었습니다.
    Term을 적극 활용하였습니다.

--- backend/src/main/java/com/univercity/unlimited/greenUniverCity/function/academic/offering/dto/CourseOfferingUpdateDTO.java
+++ backend/src/main/java/com/univercity/unlimited/greenUniverCity/function/academic/offering/dto/CourseOfferingUpdateDTO.java
@@ -1,6 +1,7 @@
 package com.univercity.unlimited.greenUniverCity.function.academic.offering.dto;
 
 
+import com.univercity.unlimited.greenUniverCity.function.academic.course.entity.Course;
 import lombok.*;
 
 @AllArgsConstructor
@@ -12,10 +13,8 @@ import lombok.*;
 public class CourseOfferingUpdateDTO {
     //** 임시 DTO 완성본 아닙니다 **
     private Long offeringId; // 강의 개설 아이디
-
-    private Long professorId; // 담당 교수 아이디
     private String courseName; // 개설 강의명
-
-    private int year; // 개설 년도
-    private String semester; // 개설 학기 ex) 1학기 2학기
+    private Long courseId; // 강의 정보
+    private Long professorId; // 담당 교수 아이디
+    private Long termId;
 }
